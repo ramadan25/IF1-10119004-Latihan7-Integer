555b37ed7f86c8f542d50447c16e9d4771ba1816
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-20 11:23:34 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan7-Integer/src/if1/pkg10119004/latihan7/integer/IF110119004Latihan7Integer.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan7.integer;
+
+/**
+ *
+ * @author ENDOG
+ * NAMA     : Muhamad Ramadan
+ * KELAS    : IF 1
+ * NIM      : 10119004
+ * Deskripsi Program : Program ini berisi untuk menampilkan
+ * value ke layar
+ */
+public class IF110119004Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
